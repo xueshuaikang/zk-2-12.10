135167f14b1ea97a8ea4dc49524f9fdddecec1d5
xuenuo <626096325@qq.com>
2018-12-10 09:59:01 +0800

    本地服务搭建完成

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,5 @@
 var express = require('express');
 var router = express.Router();
 
-let query=require("../mysql/index.js");
-let sql=require("../mysql/sql.js");
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  query(sql.SELECT_SEARCH_ALL,(err,result)=>{
-    if(err){
-      res.send(err);
-    }else{
-      res.send({code:result});
-    }
-  })
-});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+let query=require("../mysql/index.js");
+let sql=require("../mysql/sql.js");
+
+router.post('/', function(req, res, next) {
+  let data=req.body;
+  let page=data.page;
+  let pageSize=data.pageSize;
+
+  query(sql.SELECT_SEARCH_ALL,(err,result)=>{
+    if(err){
+      res.send(err);
+    }else{
+      res.send({code:result});
+    }
+  })
 });
 
 module.exports = router;
